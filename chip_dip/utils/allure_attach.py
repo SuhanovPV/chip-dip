008edcdafb0ec8f8c014b816aae2b78e76e59c75
Sukhanov Pavel <ssuxxarr@gmail.com>
2025-01-08 16:14:34 +0300

    added video attach

--- chip_dip/utils/allure_attach.py
+++ chip_dip/utils/allure_attach.py
@@ -1,12 +1,31 @@
 import allure
+import requests
+import config
 
-def page_source(browser):
+
+def add_html(browser):
     allure.attach(
         body=browser.driver.page_source,
         name='page',
         attachment_type=allure.attachment_type.HTML,
         extension='.html'
     )
+
+
+def add_xml(browser):
+    allure.attach(
+        body=browser.driver.page_source,
+        name='screen_xml_dump',
+        attachment_type=allure.attachment_type.XML
+    )
+
+
+def page_source(browser, service):
+    if service == 'web':
+        add_html(browser)
+    elif service == 'app':
+        add_xml(browser)
+
 
 def screenshot(browser):
     allure.attach(
@@ -15,3 +34,32 @@ def screenshot(browser):
         attachment_type=allure.attachment_type.PNG,
         extension='.png'
     )
+
+
+def bs_video_url(session_id):
+    conf = config.load_app_config_from_env('remote')
+    bstack_session = requests.get(
+        f'https://api.browserstack.com/app-automate/sessions/{session_id}.json',
+        auth=(conf.name, conf.accessKey)
+    ).json()
+    return bstack_session['automation_session']['video_url']
+
+
+def selenoid_video_url(session_id):
+    conf = config.load_web_config_from_env('remote')
+    return f'https://{conf.selenoid_url}/video/{session_id}.mp4'
+
+
+def video(service, session_id):
+    if service == 'app':
+        video_url = bs_video_url(session_id)
+    else:
+        video_url = selenoid_video_url(session_id)
+    allure.attach(
+        '<html><body><video width="100%" height="100%" controls autoplay>'
+        f'<source src={video_url} type="video/mp4">'
+        '</video></body></html>',
+        name='video_rec',
+        attachment_type=allure.attachment_type.HTML
+    )
+
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,14 @@
+import allure
+
 import config
 import pytest
 from selene import browser
 from chip_dip.pages.cart_page import CartPage
 from chip_dip.pages.login_form import Login
+from chip_dip.app_pages.main_page import MainPage
+from chip_dip.app_pages.login_page import LoginPage
 from chip_dip.data.Users import user
+from chip_dip.utils import allure_attach
 
 
 def pytest_addoption(parser):
@@ -18,6 +23,7 @@ def pytest_addoption(parser):
         help='--service: web, app'
     )
 
+
 @pytest.fixture(scope='class', autouse=True)
 def browser_settings(request):
     service = request.config.getoption('--service')
@@ -31,8 +37,14 @@ def browser_settings(request):
         raise KeyError
 
     yield service
+    session_id = browser.driver.session_id
+    allure_attach.screenshot(browser)
+    allure_attach.page_source(browser, service)
 
     browser.quit()
+
+    if location == 'remote':
+        allure_attach.video(service, session_id)
 
 
 @pytest.fixture()
@@ -40,11 +52,11 @@ def mark_skip_app_test(request):
     if request.config.getoption('--service') == 'web':
         pytest.skip('This test for application')
 
+
 @pytest.fixture()
 def mark_skip_web_test(request):
     if request.config.getoption('--service') == 'app':
         pytest.skip('This test for web')
-
 
 
 @pytest.fixture()
@@ -65,3 +77,16 @@ def logout():
 def clear_cart(login_if_not_auth):
     cart = CartPage()
     cart.clear_if_not_empty()
+
+
+@pytest.fixture(scope='function')
+def skip_welcome_screen():
+    main_page = MainPage()
+    if main_page.is_welcome_screen():
+        with allure.step('Skip welcome screen'):
+            main_page.scip_welcome_screen() \
+                .select_region()
+        with allure.step('Login'):
+            main_page.open_auth_page()
+            login_page = LoginPage()
+            login_page.login(user.login, user.password)
